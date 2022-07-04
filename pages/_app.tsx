afa41bdd19f358edb4ca35beeb134021fe33b3a2
Mohammad Faisal <mohammadfaisal1011@gmail.com>
2022-07-05 01:41:25 +0600

    Working with redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { wrapper } from "../store/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
---
+++ store/authSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import { AppState } from "./store";
+import { HYDRATE } from "next-redux-wrapper";
+
+export interface AuthState {
+  authState: boolean;
+}
+
+const initialState: AuthState = {
+  authState: false,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setAuthState(state, action) {
+      state.authState = action.payload;
+    },
+  },
+
+  extraReducers: {
+    [HYDRATE]: (state, action) => {
+      console.log("HYDRATE", action.payload);
+      return {
+        ...state,
+        ...action.payload.auth,
+      };
+    },
+  },
+});
+
+export const { setAuthState } = authSlice.actions;
+
+export const selectAuthState = (state: AppState) => state.auth.authState;
+
+export default authSlice.reducer;
